3bfcaaa98ae659dfa32a80a41c362f5f9d226fce
rauufaa <rauufanugerahakbar@gmail.com>
2024-05-08 11:58:43 +0700

    Test

---
+++ index.ts
@@ -0,0 +1,31 @@
+import mysql from "mysql2/promise"
+
+const connection = await mysql.createConnection({
+    host: 'database-1.crgmwa8m4s4q.ap-southeast-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'Kane3412',
+    database: 'test',
+});
+
+try {
+    const [results, fields] = await connection.query(
+      'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45'
+    );
+  
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  } catch (err) {
+    console.log(err);
+  }
+  
+  // Using placeholders
+  try {
+    const [results] = await connection.query(
+      'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+      ['Page', 45]
+    );
+  
+    console.log(results);
+  } catch (err) {
+    console.log(err);
+  }
